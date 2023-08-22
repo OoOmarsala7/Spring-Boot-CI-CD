9a38d0a945a60e03f747db546073d61d6add5549
Omar Salah <112505120+OoOmarsala7@users.noreply.github.com>
2023-08-23 02:56:53 +0300

    Add files via upload

---
+++ scripts/copy_application.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws s3 cp s3://springbootapp12/app-SNAPSHOT.jar /var
+
+
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -jar /var/app-SNAPSHOT.jar --server.port=8081 dev/null 2> /dev/null < /dev/null &  
+
